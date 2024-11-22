4ddad5c7f60cd3e7436c327d287668af139bc17f
M Rajshankar <rajshankarm1729@gmail.com>
2024-11-22 19:20:53 +0530

    Added API routes. Updated .gitignore to remove Heroku logs. Created requirements.txt and Procfile for Heroku deployment of API.

--- api.py
+++ api.py
@@ -1,4 +1,233 @@
 from flask import Flask, jsonify
+from scraper import *
+
+BASE_URL = 'https://balatrogame.fandom.com/wiki/'
+SUB_URLS = {
+    'jokers': 'Jokers',
+    'decks': 'Decks',
+    'tarot': 'Tarot_Cards',
+    'spectral': 'Spectral_Cards',
+    'planets': 'Planet_Cards',
+    'vouchers': 'Vouchers',
+    'enhancements': 'Card_Modifiers#Enhancements',
+    'editions': 'Card_Modifiers#Editions',
+    'seals': 'Card_Modifiers#Seals',
+    'stakes': 'Stakes',
+    'blinds': 'Blinds_and_Antes',
+    'tags': 'Tags'
+}
+
 
 app = Flask(__name__)
 
+# Define routes for each category to pull data for
+
+@app.route('/jokers/<int:id>', methods=['GET'])
+def jokers():
+    url = BASE_URL + SUB_URLS['jokers']
+    joker_data = fetch_joker_data(url, True)
+    joker_json = joker_data.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(joker_json)
+
+
+@app.route('/decks/<int:id>', methods=['GET'])
+def decks():
+    url = BASE_URL + SUB_URLS['decks']
+    deck_data = fetch_deck_data(url, True)
+    deck_json = deck_data.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(deck_json)
+
+
+@app.route('/tarot/<int:id>', methods=['GET'])
+def tarot():
+    url = BASE_URL + SUB_URLS['tarot']
+    tarot_data = fetch_tarot_data(url, True)
+    tarot_json = tarot_data.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(tarot_json)
+
+
+@app.route('/spectral', methods=['GET'])
+def spectral():
+    url = BASE_URL + SUB_URLS['spectral']
+    spectral_data = fetch_spectral_data(url, True)
+    spectral_json = spectral_data.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(spectral_json)
+
+
+@app.route('/planets', methods=['GET'])
+def planets():
+    url = BASE_URL + SUB_URLS['planets']
+    planet_data = fetch_planet_data(url, True)
+    planet_json = planet_data.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(planet_json)
+
+
+@app.route('/vouchers', methods=['GET'])
+def vouchers():
+    url = BASE_URL + SUB_URLS['vouchers']
+    voucher_data = fetch_voucher_data(url, True)
+    voucher_json = voucher_data.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(voucher_json)
+
+
+@app.route('/enhancements', methods=['GET'])
+def enhancements():
+    url = BASE_URL + SUB_URLS['enhancements']
+    enhancement_data = fetch_modifier_data(url, True, 0)
+    enhancement_json = enhancement_data.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(enhancement_json)
+
+
+@app.route('/editions', methods=['GET'])
+def editions():
+    url = BASE_URL + SUB_URLS['editions']
+    edition_data = fetch_modifier_data(url, True, 1)
+    edition_json = edition_data.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(edition_json)
+
+
+@app.route('/seals', methods=['GET'])
+def seals():
+    url = BASE_URL + SUB_URLS['seals']
+    seal_data = fetch_modifier_data(url, True, 2)
+    seal_json = seal_data.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(seal_json)
+
+
+@app.route('/stakes', methods=['GET'])
+def stakes():
+    url = BASE_URL + SUB_URLS['stakes']
+    stake_data = fetch_stakes_data(url, True)
+    stake_data.reset_index().rename(columns={'index': 'id'}, inplace=True)
+    stake_json = stake_data.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(stake_json)
+
+
+@app.route('/blinds', methods=['GET'])
+def blinds():
+    url = BASE_URL + SUB_URLS['blinds']
+    blind_data = fetch_blinds_data(url, True)
+    blind_data.reset_index().rename(columns={'index': 'id'}, inplace=True)
+    blind_json = blind_data.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(blind_json)
+
+
+@app.route('/tags', methods=['GET'])
+def tags():
+    url = BASE_URL + SUB_URLS['tags']
+    tag_data = fetch_tags_data(url, True)
+    tag_json = tag_data.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(tag_json)
+
+# Routes to pull individual items for a category
+
+@app.route('/jokers/<int:id>', methods=['GET'])
+def jokers(id):
+    url = BASE_URL + SUB_URLS['jokers']
+    joker_data = fetch_joker_data(url)
+    joker_info = joker_data[joker_data['id'] == id]
+    joker_json = joker_info.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(joker_json)
+
+
+@app.route('/decks/<int:id>', methods=['GET'])
+def decks(id):
+    url = BASE_URL + SUB_URLS['decks']
+    deck_data = fetch_deck_data(url)
+    deck_info = deck_data[deck_data['id'] == id]
+    deck_json = deck_info.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(deck_json)
+
+
+@app.route('/tarot/<int:id>', methods=['GET'])
+def tarot(id):
+    url = BASE_URL + SUB_URLS['tarot']
+    tarot_data = fetch_tarot_data(url)
+    tarot_info = tarot_data[tarot_data['id'] == id]
+    tarot_json = tarot_info.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(tarot_json)
+
+
+@app.route('/spectral/<int:id>', methods=['GET'])
+def spectral(id):
+    url = BASE_URL + SUB_URLS['spectral']
+    spectral_data = fetch_spectral_data(url)
+    spectral_info = spectral_data[spectral_data['id'] == id]
+    spectral_json = spectral_info.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(spectral_json)
+
+
+@app.route('/planets/<int:id>', methods=['GET'])
+def planets(id):
+    url = BASE_URL + SUB_URLS['planets']
+    planet_data = fetch_planet_data(url)
+    planet_info = planet_data[planet_data['id'] == id]
+    planet_json = planet_info.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(planet_json)
+
+
+@app.route('/vouchers/<int:id>', methods=['GET'])
+def vouchers(id):
+    url = BASE_URL + SUB_URLS['vouchers']
+    voucher_data = fetch_voucher_data(url)
+    voucher_info = voucher_data[voucher_data['id'] == id]
+    voucher_json = voucher_info.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(voucher_json)
+
+
+@app.route('/enhancements/<int:id>', methods=['GET'])
+def enhancements(id):
+    url = BASE_URL + SUB_URLS['enhancements']
+    enhancement_data = fetch_modifier_data(url, 0)
+    enhancement_info = enhancement_data[enhancement_data['id'] == id]
+    enhancement_json = enhancement_info.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(enhancement_json)
+
+
+@app.route('/editions/<int:id>', methods=['GET'])
+def editions(id):
+    url = BASE_URL + SUB_URLS['editions']
+    edition_data = fetch_modifier_data(url, 1)
+    edition_info = edition_data[edition_data['id'] == id]
+    edition_json = edition_info.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(edition_json)
+
+
+@app.route('/seals/<int:id>', methods=['GET'])
+def seals(id):
+    url = BASE_URL + SUB_URLS['seals']
+    seal_data = fetch_modifier_data(url, 2)
+    seal_info = seal_data[seal_data['id'] == id]
+    seal_json = seal_info.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(seal_json)
+
+
+@app.route('/stakes/<int:id>', methods=['GET'])
+def stakes(id):
+    url = BASE_URL + SUB_URLS['stakes']
+    stake_data = fetch_stakes_data(url)
+    stake_info = stake_data[stake_data['id'] == id]
+    stake_json = stake_info.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(stake_json)
+
+
+@app.route('/blinds/<int:id>', methods=['GET'])
+def blinds(id):
+    url = BASE_URL + SUB_URLS['blinds']
+    blind_data = fetch_blinds_data(url)
+    blind_info = blind_data[blind_data['id'] == id]
+    blind_json = blind_info.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(blind_json)
+
+
+@app.route('/tags/<int:id>', methods=['GET'])
+def tags(id):
+    url = BASE_URL + SUB_URLS['tags']
+    tag_data = fetch_tags_data(url)
+    tag_info = tag_data[tag_data['id'] == id]
+    tag_json = tag_info.to_json(orient='records')[1:-1].replace('},{', '} {')
+    return jsonify(tag_json)
+
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=False)
--- scraper.py
+++ scraper.py
@@ -13,16 +13,17 @@ SUB_URLS = {
     'enhancements': 'Card_Modifiers#Enhancements',
     'editions': 'Card_Modifiers#Editions',
     'seals': 'Card_Modifiers#Seals',
-    'stakes': 'Stickers#Stake_Stickers',
+    'stakes': 'Stakes',
     'blinds': 'Blinds_and_Antes',
     'tags': 'Tags'
 }
 
-def fetch_joker_data(url):
+def fetch_joker_data(url, asList=False):
     """
         Fetches and parses table data for Jokers from the Balatro fandom Wiki.
         Args:
             url (str): URL to fetch data from.
+            asList (bool): Whether to return only the list of elements or the descriptions as well.
         Returns:
             pd.DataFrame: DataFrame containing the table data.
     """
@@ -40,7 +41,7 @@ def fetch_joker_data(url):
     joker_table = tables[1]
 
     rows = []
-
+    names = []
     # Iterate over the rows in the table
     for tr in joker_table.find("tbody").find_all("tr")[1:]:  # Skip the header row
         cells = tr.find_all("td")
@@ -59,6 +60,7 @@ def fetch_joker_data(url):
             "Rarity": cells[4].text.strip(),
             "Type": cells[6].text.strip(),
         }
+        names.append(name_tag.text.strip())
         rows.append(joker_data)
     rows = pd.DataFrame(rows)
     # Replace the type abbreviations with full names
@@ -72,14 +74,16 @@ def fetch_joker_data(url):
         '+$': 'Economy'
     }
     rows['Type'] = rows['Type'].map(type_dict)
+    if asList:
+        return names
+    return rows.reset_index().rename(columns={'index': 'id'})
 
-    return rows
-
-def fetch_deck_data(url):
+def fetch_deck_data(url, asList=False):
     """
         Fetches and parses table data for Decks from the Balatro fandom Wiki.
         Args:
             url (str): URL to fetch data from.
+            asList (bool): Whether to return only the list of elements or the descriptions as well.
         Returns:
             pd.DataFrame: DataFrame containing the table data.
     """
@@ -97,6 +101,7 @@ def fetch_deck_data(url):
     deck_table = tables[0]
 
     rows = []
+    names = []
     for tr in deck_table.find("tbody").find_all("tr")[1:]:  # Skip the header row
         cells = tr.find_all("td")
         if len(cells) < 3:  # Ensure there are enough cells to process
@@ -113,13 +118,17 @@ def fetch_deck_data(url):
             'description': desc.text.strip(),
         }
         rows.append(deck_data)
-    return pd.DataFrame(rows)
+        names.append(name_tag.text.strip())
+    if asList:
+        return names
+    return (pd.DataFrame(rows)).reset_index().rename(columns={'index': 'id'})
 
-def fetch_tarot_data(url):
+def fetch_tarot_data(url, asList=False):
     """
         Fetches and parses table data for Tarot Cards from the Balatro fandom Wiki.
         Args:
             url (str): URL to fetch data from.
+            asList (bool): Whether to return only the list of elements or the descriptions as well.
         Returns:
             pd.DataFrame: DataFrame containing the table data.
     """
@@ -137,6 +146,7 @@ def fetch_tarot_data(url):
     tarot_table = tables[0]
 
     rows = []
+    names = []
     for tr in tarot_table.find("tbody").find_all("tr")[1:]:  # Skip the header row
         cells = tr.find_all("td")
         if len(cells) < 2:  # Ensure there are enough cells to process
@@ -153,13 +163,17 @@ def fetch_tarot_data(url):
             'description': desc.text.strip(),
         }
         rows.append(tarot_data)
-    return pd.DataFrame(rows)
+        names.append(name_tag.text.strip())
+    if asList:
+        return names
+    return (pd.DataFrame(rows)).reset_index().rename(columns={'index': 'id'})
 
-def fetch_planet_data(url):
+def fetch_planet_data(url, asList=False):
     """
         Fetches and parses table data for Planet Cards from the Balatro fandom Wiki.
         Args:
             url (str): URL to fetch data from.
+            asList (bool): Whether to return only the list of elements or the descriptions as well.
         Returns:
             pd.DataFrame: DataFrame containing the table data.
     """
@@ -178,6 +192,7 @@ def fetch_planet_data(url):
     planet_table = tables[0]
 
     rows = []
+    names = []
     for tr in planet_table.find("tbody").find_all("tr")[1:]:  # Skip the header row
         cells = tr.find_all("td")
         if len(cells) < 4:  # Ensure there are enough cells to process
@@ -198,13 +213,17 @@ def fetch_planet_data(url):
             'hand_base_score': hand_base_score.strip(),
         }
         rows.append(planet_data)
-    return pd.DataFrame(rows)
+        names.append(name_tag.text.strip())
+    if asList:
+        return names
+    return (pd.DataFrame(rows)).reset_index().rename(columns={'index': 'id'})
 
-def fetch_spectral_data(url):
+def fetch_spectral_data(url, asList=False):
     """
         Fetches and parses table data for Spectral Cards from the Balatro fandom Wiki.
         Args:
             url (str): URL to fetch data from.
+            asList (bool): Whether to return only the list of elements or the descriptions as well.
         Returns:
             pd.DataFrame: DataFrame containing the table data.
     """
@@ -223,6 +242,7 @@ def fetch_spectral_data(url):
     spectral_table = tables[0]
 
     rows = []
+    names = []
     for tr in spectral_table.find("tbody").find_all("tr")[1:]:  # Skip the header row
         cells = tr.find_all("td")
         if len(cells) < 3:  # Ensure there are enough cells to process
@@ -239,13 +259,17 @@ def fetch_spectral_data(url):
             'description': desc.text.strip(),
         }
         rows.append(spectral_data)
-    return pd.DataFrame(rows)
+        names.append(name_tag.text.strip())
+    if asList:
+        return names
+    return (pd.DataFrame(rows)).reset_index().rename(columns={'index': 'id'})
 
-def fetch_voucher_data(url):
+def fetch_voucher_data(url, asList=False):
     """
         Fetches and parses table data for Vouchers from the Balatro fandom Wiki.
         Args:
             url (str): URL to fetch data from.
+            asList (bool): Whether to return only the list of elements or the descriptions as well.
         Returns:
             pd.DataFrame: DataFrame containing the table data.
     """
@@ -263,6 +287,7 @@ def fetch_voucher_data(url):
     voucher_table = tables
 
     rows = []
+    names = []
     for tr in voucher_table.find("tbody").find_all("tr")[2:]:  # Skip the header row
         cells = tr.find_all("td")
         if len(cells) < 4:  # Ensure there are enough cells to process
@@ -270,28 +295,33 @@ def fetch_voucher_data(url):
 
         # Extract the data
         image_base = cells[0].find('a').find('img').get('data-src') or cells[0].find('a').find('img').get('src')
-        name_tag_base = cells[0].text.strip()
-        desc_base = cells[1].text.strip()
+        name_tag_base = cells[0]
+        desc_base = cells[1]
         image_upg = cells[2].find('a').find('img').get('data-src') or cells[2].find('a').find('img').get('src')
-        name_tag_upg = cells[2].text.strip() 
-        desc_upg = cells[3].text.strip()
+        name_tag_upg = cells[2] 
+        desc_upg = cells[3]
 
         voucher_data = {
             'image_base': image_base,
-            'name_base': name_tag_base,
-            'description_base': desc_base,
+            'name_base': name_tag_base.text.strip(),
+            'description_base': desc_base.text.strip(),
             'image_upg': image_upg,
-            'name_upg': name_tag_upg,
-            'description_upg': desc_upg,
+            'name_upg': name_tag_upg.text.strip(),
+            'description_upg': desc_upg.text.strip(),
         }
         rows.append(voucher_data)
-    return pd.DataFrame(rows)
+        names.append(name_tag_base.text.strip())
+        names.append(name_tag_upg.text.strip())
+    if asList:
+        return names
+    return (pd.DataFrame(rows)).reset_index().rename(columns={'index': 'id'})
 
-def fetch_modifier_data(url, idx):
+def fetch_modifier_data(url, idx, asList=False):
     """
         Fetches and parses table data for all card modifiers from the Balatro fandom Wiki.
         Args:
             url (str): URL to fetch data from.
+            asList (bool): Whether to return only the list of elements or the descriptions as well.
             idx (int): Index number indicating which modifier to fetch. 0- Enhancements, 1- Editions, 2- Seals
         Returns:
             pd.DataFrame: DataFrame containing the table data.
@@ -311,6 +341,7 @@ def fetch_modifier_data(url, idx):
     mod_table = tables[idx]
 
     rows = []
+    names = []
     for tr in mod_table.find("tbody").find_all("tr")[1:]:  # Skip the header row
         cells = tr.find_all("td")
         if len(cells) < 2:  # Ensure there are enough cells to process
@@ -329,14 +360,17 @@ def fetch_modifier_data(url, idx):
             'cost': cost,
         }
         rows.append(enhancement_data)
-    return pd.DataFrame(rows)
+        names.append(name_tag.text.strip())
+    if asList:
+        return names
+    return (pd.DataFrame(rows)).reset_index().rename(columns={'index': 'id'})
 
-
-def fetch_stakes_data(url):
+def fetch_stakes_data(url, asList=False):
     """
         Fetches and parses table data for Stakes from the Balatro fandom Wiki.
         Args:
             url (str): URL to fetch data from.
+            asList (bool): Whether to return only the list of elements or the descriptions as well.
         Returns:
             pd.DataFrame: DataFrame containing the table data.
     """
@@ -354,6 +388,7 @@ def fetch_stakes_data(url):
     stake_table = tables[0]
 
     rows = []
+    names = []
     for tr in stake_table.find("tbody").find_all("tr")[1:]:  # Skip the header row
         cells = tr.find_all("td")
         if len(cells) < 3:  # Ensure there are enough cells to process
@@ -372,13 +407,17 @@ def fetch_stakes_data(url):
             'unlocks_deck': unlocks_deck.text.strip(),
         }
         rows.append(stake_data)
-    return pd.DataFrame(rows)
+        names.append(name_tag.text.strip())
+    if asList:
+        return names
+    return (pd.DataFrame(rows)).reset_index().rename(columns={'index': 'id'})
 
-def fetch_blinds_data(url):
+def fetch_blinds_data(url, asList=False):
     """
         Fetches and parses table data for Blinds and Antes from the Balatro fandom Wiki.
         Args:
             url (str): URL to fetch data from.
+            asList (bool): Whether to return only the list of elements or the descriptions as well.
         Returns:
             pd.DataFrame: DataFrame containing the table data.
     """
@@ -396,6 +435,7 @@ def fetch_blinds_data(url):
     blind_table = tables[0]
 
     rows = []
+    names = []
     for tr in blind_table.find("tbody").find_all("tr")[1:]:  # Skip the header row
         cells = tr.find_all("td")
         if len(cells) < 5:  # Ensure there are enough cells to process
@@ -418,13 +458,17 @@ def fetch_blinds_data(url):
             'reward': reward.text.strip()
         }
         rows.append(blind_data)
-    return pd.DataFrame(rows)
+        names.append(name_tag.text.strip())
+    if asList:
+        return names
+    return (pd.DataFrame(rows)).reset_index().rename(columns={'index': 'id'})
 
-def fetch_tags_data(url):
+def fetch_tags_data(url, asList=False):
     """
         Fetches and parses table data for Tags from the Balatro fandom Wiki.
         Args:
             url (str): URL to fetch data from.
+            asList (bool): Whether to return only the list of elements or the descriptions as well.
         Returns:
             pd.DataFrame: DataFrame containing the table data.
     """
@@ -442,6 +486,7 @@ def fetch_tags_data(url):
     tag_table = tables[0]
 
     rows = []
+    names = []
     for tr in tag_table.find("tbody").find_all("tr")[1:]:  # Skip the header row
         cells = tr.find_all("td")
         if len(cells) < 3:  # Ensure there are enough cells to process
@@ -460,4 +505,8 @@ def fetch_tags_data(url):
             'ante': ante.text.strip(),
         }
         rows.append(tag_data)
-    return pd.DataFrame(rows)
+        names.append(name_tag.text.strip())
+    if asList:
+        return names
+    return (pd.DataFrame(rows)).reset_index().rename(columns={'index': 'id'})
+
